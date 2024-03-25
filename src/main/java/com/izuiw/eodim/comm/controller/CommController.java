a2b9fd408c2d44e52a6914b9fc83e36452911bad
izuiw <aswll.kr@gmail.com>
2024-03-26 00:01:47 +0900

    2024.03.25 - logback 설정 및 log level 간단 설정 : 어노테이션 인식 안됨 / logback-spring.xml 추가 설정 확인 필요

--- src/main/java/com/izuiw/eodim/comm/controller/CommController.java
+++ src/main/java/com/izuiw/eodim/comm/controller/CommController.java
@@ -1,13 +1,21 @@
 package com.izuiw.eodim.comm.controller;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+//@Slf4j
 @Controller
 public class CommController {
 
+	private final Logger log = LoggerFactory.getLogger(CommController.class);
+	
 	@GetMapping("/main")
 	public String index() {
+		log.debug("로그 호출");
+
 		
 		return "index.html";
 	}
