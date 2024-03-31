3f1cc57258b7647e314841cbceac7beb350dc2fb
izuiw <aswll.kr@gmail.com>
2024-03-31 22:18:18 +0900

    2024.03.31 -db (docker) 연동 관련 포트 충돌 이슈

--- src/main/java/com/izuiw/eodim/config/MybatisConfig.java
+++ src/main/java/com/izuiw/eodim/config/MybatisConfig.java
@@ -2,56 +2,37 @@ package com.izuiw.eodim.config;
 
 import javax.sql.DataSource;
 
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
+
+@ComponentScan(basePackages = "com.izuiw.eodim")
 @Configuration
-@PropertySource("classpath:/application.properties")
-@MapperScan(value = "com.izuiw.eodim.mapper")
+@PropertySource("classpath:/application.properties") //①
+@MapperScan(value="com.izuiw.eodim.mapper")
 public class MybatisConfig {
+	
 
-	private final ApplicationContext applicationContext;
+	    //②
+	    @Bean
+	    @ConfigurationProperties(prefix="spring.datasource.hikari")
+	    public HikariConfig hikariConfig() {
+	        return new HikariConfig();
+	    }
 
-	@Autowired
-	public MybatisConfig(ApplicationContext applicationContext) {
-		this.applicationContext = applicationContext;
+	    //③
+	    @Bean
+	    public DataSource dataSource() throws Exception {
+	        DataSource dataSource = new HikariDataSource(hikariConfig());
+	        System.out.println(dataSource.toString());
+	        return dataSource;
+	    }
 
-	}
-
-	@Bean
-	@ConfigurationProperties(prefix = "spring.datasource.hikari")
-	public HikariConfig hikariConfig() {
-		return new HikariConfig();
-	}
-
-	@Bean
-	public DataSource dataSource() throws Exception {
-		DataSource dataSource = new HikariDataSource(hikariConfig());
-		return dataSource;
-	}
-
-	@Bean
-	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
-		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-		sqlSessionFactoryBean.setDataSource(dataSource);
-		sqlSessionFactoryBean.setTypeAliasesPackage("com.izuiw.eodim.domain");	//매퍼에서 사용 할 result, parameterType 패키지 경로 
-		sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath:/mapper/**/*.xml"));	 //쿼리 파일 경로 : src/main/resources/~
-		return sqlSessionFactoryBean.getObject();
-	}
-
-	@Bean
-	public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
-		return new SqlSessionTemplate(sqlSessionFactory);
-	}
 }
